3d29557a3389be8477cf61f4b7827c1355eba9d5
Satoxehclim <xehclim1@gmail.com>
2025-04-27 22:54:08 -0600

    Ajustes finales

--- memoria_de_instrucciones.vhd
+++ memoria_de_instrucciones.vhd
@@ -10,8 +10,8 @@ entity memoria_de_instrucciones is
     );
 end memoria_de_instrucciones;
 
-architecture Behavioral of memoria_de_instrucciones is type rom_type is array (0 to 511) of STD_LOGIC_VECTOR(31 downto 0);
-
+architecture Behavioral of memoria_de_instrucciones is 
+type rom_type is array (0 to 511) of STD_LOGIC_VECTOR(31 downto 0);
     impure function init_rom_from_file(filename : string) return rom_type is
         file rom_file : text open read_mode is filename;
         variable rom_line : line;
@@ -23,10 +23,9 @@ architecture Behavioral of memoria_de_instrucciones is type rom_type is array (0
                 read(rom_line, rom_value);
                 temp_rom(i) := to_stdlogicvector(rom_value);
             end loop;
-            return temp_rom;
-        end function;
-
-        signal rom : rom_type := init_rom_from_file("datos.bin");
+        return temp_rom;
+    end function;
+    signal rom : rom_type := init_rom_from_file("datos.bin");
 begin
     instruccion <= rom(to_integer(unsigned(pc_actual)));
 end Behavioral;
